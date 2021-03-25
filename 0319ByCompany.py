058c32d51cd41d4229ace4cef2bb1625a2dbd8c5
FlyU <974162410@qq.com>
2021-03-25 17:15:46 +0800

    3月19日爬取Feed威胁情报

---
+++ 0319ByCompany.py
@@ -0,0 +1,88 @@
+import time
+import pandas as pd
+from selenium import webdriver
+
+
+def save2csv(dic_list, n):
+    filename = "0319Company/0319Company%d.csv" % n
+    print("创建文件"+filename)
+    pd.DataFrame(dic_list).to_csv(filename, encoding='utf-8-sig')
+
+# 创建浏览器对象
+browser = webdriver.Chrome(executable_path=r'C:\Program Files (x86)\chromedriver.exe')
+
+# 向浏览器发送请求
+browser.get("https://feed.watcherlab.com/index/apt")
+
+# 等待3秒
+time.sleep(3)
+
+buttun = browser.find_element_by_xpath("//div[@class='ant-tabs-nav ant-tabs-nav-animated']/div/div[7]")
+buttun.click()
+# browser.execute_script("arguments[0].click();", apt_buttun)
+n = 12
+id = 1
+dic_list = []
+for i in range(1, 222):
+    if i < 23:  # 跳过前x个
+        continue
+    # 进入页面，定位信息
+    # time.sleep(3)
+    country_btn = browser.find_element_by_xpath("//*[@id='root']/div/div[2]/div/div[2]/div/div[1]/div[3]/div[7]/span[%d]" % i)
+    time.sleep(1)
+    browser.execute_script("arguments[0].click();", country_btn)
+    time.sleep(1)
+
+    Company = browser.find_element_by_xpath("//*[@id='root']/div/div[2]/div/div[2]/div/div[1]/div[3]/div[7]/span[%d]" % i).text
+    # time.sleep(3)
+    # list = browser.find_elements_by_xpath("//div[@class='ant-collapse-item']")
+
+    list = browser.find_elements_by_xpath("//div[@class='ant-typography ant-typography-ellipsis ant-typography-ellipsis-single-line']")
+    # report_list = browser.find_elements_by_xpath("//div[@class='ant-collapse ant-collapse-icon-position-left']")
+    flag = True
+    c = 1  # 用来定位
+    for l in list:
+        if flag:  # 首次进入，关闭所有标签，以统一动作
+            browser.execute_script("arguments[0].click();", l)
+            flag = False
+        # print(l.text)
+        time.sleep(1)
+        browser.execute_script("arguments[0].click();", l)
+        time.sleep(3)
+        content = browser.find_element_by_xpath("//div[@class='ant-collapse-content-box']")
+        rl = content
+        r_title = rl.find_element_by_xpath("//div[@class='ant-typography ant-typography-ellipsis ant-typography-ellipsis-single-line']").text
+        #                                  //*[@id="root"]/div/div[2]/div/div[2]/div/div[2]/div/div[1]/div/div/div[1]/div/div[1]/div[2]/div/div[1]/div
+        p_time = rl.find_element_by_xpath("//*[@id='root']/div/div[2]/div/div[2]/div/div[2]/div/div[1]/div/div/div[1]/div/div[%d]/div[2]/div/div[1]/div" % c).text
+        #                                     //*[@id="root"]/div/div[2]/div/div[2]/div/div[2]/div/div[1]/div/div/div[1]/div/div[1]/div[2]/div/div[2]/div
+        p_company = rl.find_element_by_xpath("//*[@id='root']/div/div[2]/div/div[2]/div/div[2]/div/div[1]/div/div/div[1]/div/div[%d]/div[2]/div/div[2]/div" % c).text
+        #                                       //*[@id="root"]/div/div[2]/div/div[2]/div/div[2]/div/div[1]/div/div/div[1]/div/div[1]/div[2]/div/div[4]/div
+        attack_tips = rl.find_element_by_xpath("//*[@id='root']/div/div[2]/div/div[2]/div/div[2]/div/div[1]/div/div/div[1]/div/div[%d]/div[2]/div/div[4]/div" % c).text
+        #                                   //*[@id="root"]/div/div[2]/div/div[2]/div/div[2]/div[2]/div[1]/div/div/div[1]/div/div[2]/div[2]/div/div[7]
+        #                                   //*[@id="root"]/div/div[2]/div/div[2]/div/div[2]/div[2]/div[1]/div/div/div[1]/div/div[1]/div[2]/div/div[7]/span
+        apt_nickname = rl.find_element_by_xpath("//*[@id='root']/div/div[2]/div/div[2]/div/div[2]/div/div[1]/div/div/div[1]/div/div[%d]/div[2]/div/div[5]/div" % c).text
+        try:
+            expand_btn = rl.find_element_by_xpath("//a[@class='ant-typography-expand']")
+            if expand_btn:
+                browser.execute_script("arguments[0].click();", expand_btn)
+        except:
+            pass
+        #                                   //*[@id="root"]/div/div[2]/div/div[2]/div/div[2]/div/div[1]/div/div/div[1]/div/div[1]/div[2]/div/div[7]/span
+        details = rl.find_element_by_xpath("//*[@id='root']/div/div[2]/div/div[2]/div/div[2]/div/div[1]/div/div/div[1]/div/div[%d]/div[2]/div/div[7]" % c).text
+        c += 1
+        dic = {
+            '机构': Company,
+            '报告标题': l.text,
+            '披露时间': p_time,
+            '披露机构': p_company,
+            '攻击方法': attack_tips,
+            '组织别名': apt_nickname,
+            '报告摘要': details,
+        }
+        print(dic)
+        dic_list.append(dic)
+    print("+++++++++++++++++++")
+    if i % 2 == 0 or i == 221:
+        save2csv(dic_list, n)
+        n += 1
+
---
+++ data2json/data2json.py
@@ -0,0 +1,40 @@
+import os
+import json
+import pandas as pd
+
+
+
+
+if "__name__" == "__main__":
+    # 1. 读取目录
+    path = os.listdir()
+    print(path)
+
+
+
+
+
+"""
+detailList = []
+attackMethod = []
+detailDict = {
+    "报告标题":reportTitle,
+    "披露时间":exploreTime,
+    "披露机构":exploreAgen,
+    "攻击手法":attackMethod,
+    "攻击国家":attackCountry,
+    "攻击工具":attackTools,
+    "报告摘要":reportBrief,
+}
+
+
+{
+    id:{
+        "组织名称":name,
+        "组织别名":nickname
+        "归属国家":country,
+        "归并":detailList
+    }
+}
+"""
+
